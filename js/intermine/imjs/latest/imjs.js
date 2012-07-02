c87e935f59333096c2bf121e2e405633dd19b237
Alex Kalderimis <alex@flymine.org>
2012-07-02 19:11:10 +0100

    Fix for path displayName overwriting bug.

--- js/intermine/imjs/latest/imjs.js
+++ js/intermine/imjs/latest/imjs.js
@@ -200,7 +200,7 @@
         if (_(attr).isString()) {
             attr = type.fields[attr];
         }
-        var data = _.extend({}, this, {descriptors: this.descriptors.concat([attr])});
+        var data = _.extend({}, this, {descriptors: this.descriptors.concat([attr]), displayName: null});
         return new PathInfo(data);
     };
 
