b44cb991e0534d7a456d5290c137e541bbb7fc1d
Alex Kalderimis <alex@flymine.org>
2012-07-03 09:37:41 +0100

    Fix alias retention bug.

--- js/intermine/imjs/latest/imjs.js
+++ js/intermine/imjs/latest/imjs.js
@@ -191,7 +191,7 @@
         if (this.isRoot()) {
             throw "Root paths do not have a parent";
         }
-        var data = _.extend({}, this, {descriptors: _(this.descriptors).initial()});
+        var data = _.extend({}, this, {descriptors: _(this.descriptors).initial(), displayName: null});
         return new PathInfo(data);
     };
 
