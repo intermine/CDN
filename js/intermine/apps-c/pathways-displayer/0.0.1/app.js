7f91c355ce1732f7ec5fc113b213fa48cf177941
Joshua Heimbach <joshkh@gmail.com>
2013-12-09 17:39:28 +0000

    CSS Changes.

--- js/intermine/apps-c/pathways-displayer/0.0.1/app.js
+++ js/intermine/apps-c/pathways-displayer/0.0.1/app.js
@@ -723,8 +723,8 @@
       	<div class="pwayWrapper"> \
       		<div class="pwayMain"> \
       			<div id="pwayHeadersContainer"></div> \
-      			<div id="pwayResultsContainer" class="collection-table"> \
-      				<div class="dataPane"> \
+      			<div id="pwayResultsContainer"> \
+      				<div class="dataPane"></div> \
       			</div> \
       			<div id="statusBar"><div class="loading-spinner">Querying mines...</div> \
       		</div> \
