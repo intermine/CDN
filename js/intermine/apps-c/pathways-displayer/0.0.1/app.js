fd1f0d647d98534e2fc655db0f4613f34ebc2ee7
Joshua Heimbach <joshkh@gmail.com>
2013-12-09 16:23:33 +0000

    Removing CSS dependncies.

--- js/intermine/apps-c/pathways-displayer/0.0.1/app.js
+++ js/intermine/apps-c/pathways-displayer/0.0.1/app.js
@@ -723,7 +723,7 @@
       	<div class="pwayWrapper"> \
       		<div class="pwayMain"> \
       			<div id="pwayHeadersContainer"></div> \
-      			<div id="pwayResultsContainer"> \
+      			<div id="pwayResultsContainer" class="collection-table"> \
       				<div class="dataPane"></div> \
       			</div> \
       			<div id="statusBar"><div class="loading-spinner">Querying mines...</div> \
