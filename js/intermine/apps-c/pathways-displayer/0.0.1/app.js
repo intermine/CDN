8c5ebd2d447cafe25050947e0a7b7d47e024a7f7
Joshua Heimbach <joshkh@gmail.com>
2013-12-15 14:46:04 +0000

    Added loading animation.

--- js/intermine/apps-c/pathways-displayer/0.0.1/app.js
+++ js/intermine/apps-c/pathways-displayer/0.0.1/app.js
@@ -845,6 +845,7 @@
       
            var loadingTemplate = require('../templates/loading');
             this.$("#pwayResultsContainer").append(loadingTemplate);
+            console.log("Loading template:" + loadingTemplate);
       
            Q.when(Helper.launchAll(params.gene, friendlyMines))
             .then(function(results) { return console.log(results) })
